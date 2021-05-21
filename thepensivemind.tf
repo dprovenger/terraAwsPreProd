fcbe0a694f21c9404ed3c216a1bc9bb54356d7c5
Jorge Arrieta <jorge.r.arrieta.jr@gmail.com>
2021-05-21 01:03:40 -0500

    filtering for redhat8 ami

--- thepensivemind.tf
+++ thepensivemind.tf
@@ -10,3 +10,13 @@ terraform {
 provider "aws" {
     region = "us-east-2"
 }
+
+# Finding and filtering out Redhat 8 image
+data "aws_ami" "app" {
+    most_recent = true
+    filter {
+      name = "name"
+      values = ["RHEL_HA-8.4.0_HVM-20210504-x86_64-2-*"]
+    }
+    owners = ["309956199498"] # RedHat images
+}
